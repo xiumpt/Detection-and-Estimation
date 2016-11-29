05af91ad79e50b123cf77f10ae4175b3e9eec052
xiumpt <dylanx05@gmail.com>
2016-11-29 22:21:50 +0800

    w8_HW.m

--- w8_HW.m
+++ w8_HW.m
@@ -1,7 +1,10 @@
 % ==========================
-% Estimation of time delay
-% estimates the delay time of a square pulse embedded in WGN.
-%
+% HW8: Maximum Likelihood Estimator
+% Estimation of time delay: estimates the delay time of a square pulse embedded in WGN.
+% Consider a signal that is a Gaussian pulse whose time delay is n0 = 50
+% WGN with variance sig2u = 0.25
+% After implementing the running correlator, the function J(n0) to be
+% maximized is shown in fig.
 % ==========================
 
 clear all; close all;
@@ -10,15 +13,13 @@ M=20;
 n=200;  % time sample
 sig2u=0.25;  % variance
 n0=50;  % time delay
-s=ones(M,1);  % generate square pulse
+s=ones(M,1);  % square pulse
 ss=[zeros(n0,1);s;zeros(n-n0-M,1)];  % embed signal in array of length N
                                     % [50*(0) 20*(1) 130*(0)]'
 x=ss+0.5*randn(length(ss),1);  % add noise to signal
 n1=1:200;
-%plot(n1,ss);figure;
 plot(n1,x);
-xlabel('n');ylabel('x[n]');title('noise');
-
+xlabel('n');ylabel('x[n]');title('signal+noise');
 
 lx=length(x);
 ls=length(s);
@@ -34,10 +35,6 @@ n0hat %estimate of delay time
 n0=0:180;
 
 figure;
-
 plot(n0,J);hold on;grid;
 line([n0hat,n0hat],[min(J),max(J)]);    %([from_x,end_x], [from_y,end_y])
 xlabel('n0');ylabel('J(n0)');
-%axis([0,180,-4,25]);
-
-
